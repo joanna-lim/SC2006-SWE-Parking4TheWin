6ba9aa65862a88ba8ac997baf90365f9797b03f4
ivanyjq <iyuen001@ntu.edu.sg>
2023-03-03 00:54:18 +0800

    for vetting

--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from flask_login import login_required, current_user
 from . import db
 from .auth import role_required
 from functools import wraps
+from .models import *
 import json
 import os
 
@@ -20,8 +21,28 @@ def home():
 @views.route('/coe-registration', methods=['GET', 'POST'])
 @role_required('driver')
 def coe_registration():
+    if request.method == "POST":
+        print(request.form["name"])
+        print(request.form["car_plate"])
+        print(request.form["coe_expiry"])
+        name = request.form["name"]
+        car_plate = request.form["car_plate"]
+        coe_expiry = request.form["coe_expiry"]
 
+        new_car = Vehicle(car_plate=car_plate)
+        db.session.add(new_car) 
+        db.session.commit()
+        print(new_car.id)
+        print(Vehicle.query.all())
+        car = Vehicle.query.all()
+        return render_template("coe_registered.html", user=current_user, car=car)
     return render_template("coe_registration.html", user=current_user)
+
+@views.route('/coe-registration', methods=['GET', 'POST'])
+@role_required('driver')
+def coe_registered():
+    car = Vehicle.query.all()
+    return render_template("coe_registered.html", user=current_user, car=car)
 
 @views.route('/view-rewards', methods=['GET', 'POST'])
 @role_required('driver')
